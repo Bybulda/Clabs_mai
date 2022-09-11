820a6b6e6316814a8cd280d7ef55ef7fb6068f2b
Nikita <kakeino@yandex.ru>
2022-09-11 16:18:09 +0300

    some functions

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
 #include "math.h"
 
+
+int atoi(char s[])
+{
+    int i, n;
+    n = 0;
+    for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
+        n = (n * 10) + (s[i] - '0');
+    return n;
+}
+
+int ifcommon(int n){
+    int step = pow(n, 0.5) + 1;
+    for (int i = 3; i*i < step; i+=2){
+        if (!n%i){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int powers(int n){
+    printf("1 ");
+    for (int i = 2; i<=n;i++){
+        if (i >9){
+            printf("%lld ", (long long) pow(i, i));
+        }
+        else{
+            printf("%d ", (int) pow(i, i));
+        }
+    }
+}
+
 int sum(int n){
     if (n == 1){
         return 1;
@@ -8,7 +40,17 @@ int sum(int n){
     return n + sum(n - 1);
 }
 
-int common(int n){
+int kratn(int n){
+    int flag = 0;
+    for (int i = 2; i < 100; i++){
+        if (!i%n){
+            flag = 1;
+            printf("%d ", i);
+        }
+    }
+    if (!flag){
+        printf("There were nothing to find");
+    }
     return 0;
 }
 
@@ -39,7 +81,8 @@ unsigned fact(unsigned int num){
 int main() {
     int n;
     scanf("%d", &n);
-    int b = sum(n);
-    printf("%d\n", b);
+    powers(n);
+//    int b = sum(n);
+//    printf("%d\n", b);
     return 0;
 }
