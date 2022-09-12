43ebfc44477c39a57ecc20dc1717216eb1b1900a
Nikita <kakeino@yandex.ru>
2022-09-12 20:39:00 +0300

    A "maybe" working main

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include "math.h"
+#include "string.h"
 
 
-int atoi(char *s)
+int toi(char *s)
 {
     int i, n;
     n = 0;
@@ -31,9 +32,10 @@ int ifcommon(int n){
     double step = pow(n, 0.5) + 1;
     for (int i = 2; i < (int) step; i+=2){
         if (!(n%i)){
-            return 1;
+            printf("Yeah, this one is composite number\n");
         }
     }
+    printf("This is a common number\n");
     return 0;
 }
 
@@ -75,7 +77,7 @@ int sum(int n){
 }
 
 
-unsigned fact(unsigned int num){
+long long fact(long long num){
     if (num == 1){
         return num;
     }
@@ -83,9 +85,29 @@ unsigned fact(unsigned int num){
 }
 
 
-int main() {
+int main(int argc, char *argv[]) {
     int n;
-    scanf("%d", &n);
-    printf("%d", ifcommon(n));
+    if (argc<3){
+        printf("U must enter a number and a flag!\nExample: <number> </p or -p>\n");
+    }
+    else if (argc == 3 && (!strcmp(argv[2], "-h") || !strcmp(argv[2], "/h"))){
+        n = toi(argv[1]);
+        kratn(n);
+    }
+    else if (argc == 3 && (!strcmp(argv[2], "-p") || !strcmp(argv[2], "/p"))){
+        ifcommon(toi(argv[1]));
+    }
+    else if (argc == 3 && (!strcmp(argv[2], "-s") || !strcmp(argv[2], "/s"))){
+        split(toi(argv[1]));
+    }
+    else if (argc == 3 && (!strcmp(argv[2], "-e") || !strcmp(argv[2], "/e"))){
+        powers(toi(argv[1]));
+    }
+    else if (argc == 3 && (!strcmp(argv[2], "-a") || !strcmp(argv[2], "/a"))){
+        printf("The sum is %d", sum(toi(argv[1])));
+    }
+    else if (argc == 3 && (!strcmp(argv[2], "-f") || !strcmp(argv[2], "/f"))){
+        printf("The factorial is %lld", fact(toi(argv[1])));
+    }
     return 0;
 }
