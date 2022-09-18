6ac1b3ab690d11b57faaf95fd214410644b664f5
Nikita <kakeino@yandex.ru>
2022-09-18 19:05:05 +0300

    Seventh minifix

--- Seventh_task/main.c
+++ Seventh_task/main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include "time.h"
-#include "math.h"
-#include "limits.h"
 #include "stdlib.h"
 
 
-void find_min_max(int *arr){
+void find_min_max(int size, int *arr){
     int imx = 0, imn = 0, mn = arr[0], mx = arr[0];
-    for (int i = 0; i < 64; i++){
+    for (int i = 0; i < size; i++){
         if (arr[i] > mx){
             mx = arr[i];
             imx = i;
@@ -23,9 +21,9 @@ void find_min_max(int *arr){
     arr[imn] = mx;
 }
 
-void arr_print(int *arr){
-    for(int i = 0; i < 64; i++){
-        if (!(i % 8)){
+void arr_print(int size, int *arr){
+    for(int i = 0; i < size; i++){
+        if (!(i % 5)){
             printf("\n");
         }
         printf("%d ", arr[i]);
@@ -33,17 +31,20 @@ void arr_print(int *arr){
 }
 
 int main() {
-    int n[64];
+    printf("Please enter the size of array: n = ");
+    int size;
+    scanf("%d", &size);
+    int n[size];
     srand(time(NULL));
-    for (int i = 0; i < 64; i++){
-        if (!(i % 8)){
+    for (int i = 0; i < size; i++){
+        if (!(i % 5)){
             printf("\n");
         }
         n[i] = rand();
         printf("%d ", n[i]);
     }
     printf("\n\n");
-    find_min_max(n);
-    arr_print(n);
+    find_min_max(size, n);
+    arr_print(size, n);
     return 0;
 }
