f97294b2d0c87d3adc10df0a30c1ea295fdc963b
Nikita <kakeino@yandex.ru>
2022-09-19 12:35:25 +0300

    Big changes

--- First_lab/Second_task/main.c
+++ First_lab/Second_task/main.c
@@ -3,14 +3,18 @@
 #include "string.h"
 #include "limits.h"
 #include "stdlib.h"
+#include "ctype.h"
 
 
-int toi(char *s){
-    int n = 0;
+ int toi(char *s){
+    int n = 0, neg = 1;
     for(int i = 0; s[i] != '\0'; i++){
-        if (n > INT_MAX/10){
+        if (n > UINT_MAX/10){
             printf("Overflow error, try lower number\n");
             exit(0);
+        }
+        if (s[i] == '-'){
+            neg = -1;
         }
         if (s[i] - '0' < 0 || s[i] - '9' > 9){
             printf("Not a number\n");
@@ -18,12 +22,35 @@ int toi(char *s){
         }
         n = n * 10 + (s[i] - '0');
     }
-    return n;
+    return neg * n;
 }
 
 
+double tof(char *s){
+    return 1.9;
+}
+
+
+int int_or_float(char *s){
+    int flag = 0;
+    while(*s){
+        if (!isdigit(*s)){
+            if (*s == '.' || *s == ','){
+                flag = 1;
+                break;
+            }
+            if (isalpha(*s)) {
+                printf("Please use numbers!!\n");
+                exit(0);
+            }
+        }
+        *s++;
+    }
+    if (!flag) return 1;
+    else return 0;
+}
 double find_x(double b, double c){
-    return -1. * (c*c) / b;
+    return -1. * (c) / b;
 }
 
 
@@ -47,20 +74,22 @@ void kratn(int a, int b){
 }
 
 
-void if_triangle(int a, int b, int c){
-    if ((a + b > c && c > a && c > b) || (a + c > b && b > c && b > a)
-    || (c + b > a && a > c && a > b)){
-        printf("Yes it can be a triangle\n");
+void if_triangle(double a, double b, double c){
+    a *= a;
+    b *= b;
+    c *= c;
+    if (a + b == c || a + c == b || b + c == a){
+        printf("Yes it can be a right triangle\n");
     }
     else{
-        printf("No, it`s not a triangle\n");
+        printf("No, it`s not a right triangle\n");
     }
 }
 
 
 int main(int argc, char *argv[]) {
     int n;
-    printf("%d\n", argc);
+//    printf("%d\n", argc);
     if (argc<4){
         printf("U must enter a number and a flag!\nExample: <number> </q or -q>\n");
     }
@@ -69,7 +98,14 @@ int main(int argc, char *argv[]) {
 //        kratn(n);
     }
     else if (argc == 4 && (!strcmp(argv[1], "-m") || !strcmp(argv[1], "/m"))){
-        kratn(toi(argv[2]), toi(argv[3]));
+        if (int_or_float(argv[2]) && int_or_float(argv[3])){
+            kratn(toi(argv[2]), toi(argv[3]));
+        }
+        else{
+            printf("You cant use float numbers bro!\n");
+            exit(0);
+        }
+
     }
     else if (argc == 5 && (!strcmp(argv[1], "-t") || !strcmp(argv[1], "/t"))){
         if_triangle(toi(argv[2]), toi(argv[3]), toi(argv[4]));
