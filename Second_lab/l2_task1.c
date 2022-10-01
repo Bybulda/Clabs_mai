12b9c97649a83d1eab4c999ca316e334bfe7a461
Nikita <kakeino@yandex.ru>
2022-10-01 13:28:52 +0300

    sec lab

---
+++ Second_lab/l2_task1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void print_array(int* arr, int size){}
+
+void fill_array(int* arr, int size){}
+
+int main(){
+    srand(time(NULL));
+    int n, m;
+    printf("Please, enter the size of the first array: ");
+    scanf("%d", &n);
+    printf("\nPlease, enter the size of the second array: ");
+    scanf("%d", &m);
+}
