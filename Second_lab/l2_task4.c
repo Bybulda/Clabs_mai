903874d4d75406e7e14d5231a71b80bea655438a
Nikita <kakeino@yandex.ru>
2022-10-06 11:23:59 +0300

    lab2 tasks 4 and 5 ready

---
+++ Second_lab/l2_task4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <math.h>
+
+
+double fpower(double n, int pow){
+    if (pow == 0)
+        return 1;
+    else if (pow < 0)
+        return fpower(n, pow + 1) * (1. / n);
+    else
+        return fpower(n, pow - 1) * n;
+}
+
+
+double avg_geom(int n, ...){
+    double res = 1;
+    va_list l;
+    va_start(l, n);
+    for (int i = 0; i < n; i++) {
+        res *= va_arg(l, double);
+    }
+    va_end(l);
+    if (res < 0){
+        return NAN;
+    }
+    return fpower(res, -n);
+}
+
+int main(int argc, char* argv[]){
+    int com;
+    printf("Please enter the command\n1) Find the a geometric mean\n2) Find a number in an integer power\n");
+    scanf("%d", &com);
+    if (com == 1){
+        return 1; // Как вызвать функцию???
+    }
+    else if(com == 2){
+        int pw;
+        double n;
+        printf("Please enter a number and a power\n");
+        scanf("%lf", &n);
+        scanf("%d", &pw);
+        printf("The result is: %lf", fpower(n, pw));
+
+    }
+    return 0;
+}
---
+++ Second_lab/l2_task5.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int cmp(const void * x, const void * y){
+    return ( *(int*)x - *(int*)y );
+}
+
+void arr_print(int size, int *arr){
+    printf("\n");
+    for(int i = 0; i < size; i++){
+        if (!(i % 5)){
+            printf("\n");
+        }
+        printf("%d ", arr[i]);
+    }
+}
+
+
+int fill_array(int* arr, int size, int a, int b){
+    int c = b - a + 1;
+    for (int i = 0; i < size; i++){
+        arr[i] = rand() % (c) + a;
+    }
+    return 0;
+}
+
+int swap(int* mx, int* mn){
+    int buff = *mx;
+    *mx = *mn;
+    *mn = buff;
+    return 0;
+}
+
+
+int find_min_max(int size, int *arr){
+    int imx = 0, imn = 0, mn = arr[0], mx = arr[0];
+    for (int i = 0; i < size; i++){
+        if (arr[i] > mx){
+            mx = arr[i];
+            imx = i;
+        }
+        if (arr[i] < mn){
+            mn = arr[i];
+            imn = i;
+        }
+    }
+    swap(&arr[imx], &arr[imn]);
+    return 0;
+}
+
+
+int fill_unique(int* arr1, int*arr2, int size, int* size_sec){
+    qsort(arr1, size, sizeof(int), cmp);
+    int* buff = size_sec;
+    int count = 0;
+    for (int i = 0; i < size; i++){
+        if (i < size - 1 && arr1[i] != arr1[i + 1] || i == size - 1 && arr1[i - 1] != arr1[i]){
+            arr2[count++] = arr1[i];
+        }
+        else{
+            i++;
+        }
+    }
+    *buff = count;
+    return 0;
+}
+
+
+int main(){
+    srand(time(NULL));
+    int n, a, b, flag;
+    printf("Please, enter the size of array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("\nPlease enter the range [a, b]\nEnter a = ");
+    scanf("%d", &a);
+    printf("\nEnter b = ");
+    scanf("%d", &b);
+    fill_array(arr, n, a, b);
+    printf("\nPlease, enter the number of command\n1) Swap min and max elements\n2) Make an array of unique elements\nFlag = ");
+    scanf("%d", &flag);
+    if (flag == 1){
+        arr_print(n, arr);
+        find_min_max(n, arr);
+        arr_print(n, arr);
+    }
+    else if (flag == 2){
+        int arr1[n], counter = 0;
+        fill_unique(arr, arr1, n, &counter);
+        arr_print(n, arr);
+        arr_print(counter, arr1);
+    }
+    return 0;
+}
